b3686f7d867cbe4f600d51569476e8b40d0ca51e
Taylor Rettke <taylor.rettke@gmail.com>
2015-05-01 17:17:10 +1000

    Make 'expand' true but 'dot' false - test result

--- Gruntfile.js
+++ Gruntfile.js
@@ -391,7 +391,7 @@ module.exports = function (grunt) {
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
         }, {
-          expand: false,
+          expand: true,
           //dot: true,
           cwd: '.',
           src: 'bower_components/font-awesome/fonts/*',
